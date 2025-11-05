885ad781431f0039028f45b319f708b31deef13a
Trym Silsand Gerhardsen <trymsg@stud.ntnu.no>
2025-11-05 17:26:05 +0100

    add buildings

--- src/components/MapLibreMap.tsx
+++ src/components/MapLibreMap.tsx
@@ -1,13 +1,19 @@
 import { SearchBar, type Address } from './SearchBar';
 import { LngLat, type MapLayerMouseEvent } from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import { RMap, useMap } from 'maplibre-react-components';
+import { RLayer, RMap, RSource, useMap } from 'maplibre-react-components';
 import { getHoydeFromPunkt } from '../api/getHoydeFromPunkt';
 import { useEffect, useState } from 'react';
 import { Overlay } from './Overlay';
 import DrawComponent from './DrawComponent';
+import { getBygningAtPunkt } from '../api/getBygningAtPunkt';
+import type { GeoJSON } from 'geojson';
 
 const TRONDHEIM_COORDS: [number, number] = [10.40565401, 63.4156575];
+const polygonStyle = {
+  'fill-outline-color': 'rgba(0,0,0,0.1)',
+  'fill-color': 'rgba(18, 94, 45, 0.41)',
+};
 
 export const MapLibreMap = () => {
   const [pointHoyde, setPointHoydeAtPunkt] = useState<number | undefined>(
@@ -15,12 +21,23 @@ export const MapLibreMap = () => {
   );
   const [address, setAddress] = useState<Address | null>(null);
   const [clickPoint, setClickPoint] = useState<LngLat | undefined>(undefined);
+  const [bygningsOmriss, setBygningsOmriss] = useState<GeoJSON | undefined>(
+    undefined
+  );
 
   useEffect(() => {
     console.log(pointHoyde, clickPoint);
   }, [clickPoint, pointHoyde]);
 
   const onMapClick = async (e: MapLayerMouseEvent) => {
+    const bygningResponse = await getBygningAtPunkt(e.lngLat.lng, e.lngLat.lat);
+    if (bygningResponse?.FkbData?.BygningsOmriss) {
+      const geoJsonObject = JSON.parse(bygningResponse.FkbData.BygningsOmriss);
+      setBygningsOmriss(geoJsonObject);
+    } else {
+      setBygningsOmriss(undefined);
+    }
+
     const hoyder = await getHoydeFromPunkt(e.lngLat.lng, e.lngLat.lat);
     setPointHoydeAtPunkt(hoyder[0].Z);
     setClickPoint(new LngLat(e.lngLat.lng, e.lngLat.lat));
@@ -40,9 +57,20 @@ export const MapLibreMap = () => {
       <Overlay>
         <h2>Dette er et overlay</h2>
         <p>Legg til funksjonalitet knyttet til kartet.</p>
-        <SearchBar setAddress={setAddress}/>
+        <SearchBar setAddress={setAddress} />
       </Overlay>
       <DrawComponent />
+      {bygningsOmriss && (
+        <>
+          <RSource id="bygning" type="geojson" data={bygningsOmriss} />
+          <RLayer
+            source="bygning"
+            id="bygning-fill"
+            type="fill"
+            paint={polygonStyle}
+          />
+        </>
+      )}
     </RMap>
   );
 };
